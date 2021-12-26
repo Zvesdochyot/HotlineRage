81ac22fd4a1ac87d49b1c5bb3f66f7c436538bb4
Rostyslav Nakarlovich <zvesdochyot@gmail.com>
2021-12-27 01:48:58 +0200

    Finished client and server-side, extended map

--- game.py
+++ game.py
@@ -1,25 +1,72 @@
+import sys
+import socket
 import random
+import pickle
+from os import system
 from time import time
 from math import atan2, degrees, sin, cos, radians
 
 import pygame
 from pygame.locals import K_w, K_a, K_s, K_d, K_i
 
+
+def get_angle(point_a, point_b):
+    changeInX = point_b[0] - point_a[0]
+    changeInY = point_b[1] - point_a[1]
+    return degrees(atan2(changeInY, changeInX))
+
+
+def send_to_server(client, data, server):
+    client.sendto(pickle.dumps(data), server)
+
+
+def receive_from_server(server):
+    try:
+        data, addr = server.recvfrom(2048)
+        return pickle.loads(data)
+    except BlockingIOError:
+        return None
+
+
+def size_in_percent(percent, horizontal=True):
+    return int(SCREEN_SIZE[int(horizontal)] * (percent / 100))
+
+
+def win_screen(screen, winner):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+        text_surface = font.render(f"Winner: {winner}", True, (255, 255, 255))
+
+        size = pygame.display.get_surface().get_size()
+
+        screen.blit(text_surface,
+                    (int((size[0] - text_surface.get_width()) // 2), int((size[1] - text_surface.get_height()) // 2)))
+
+        pygame.display.flip()
+
+        elapsed = clock.tick(FPS) / 1000
+        pygame.display.set_caption(str(elapsed))
+
+
+pygame.init()
 SCREEN_SIZE = [600, 600]
+elapsed = 0
+FPS = 200
+RUNNING = True
 
-
-def get_angle(pointA, pointB):
-    changeInX = pointB[0] - pointA[0]
-    changeInY = pointB[1] - pointA[1]
-    return degrees(atan2(changeInY,changeInX))
+screen = pygame.display.set_mode(SCREEN_SIZE)
+font = pygame.font.Font(pygame.font.get_default_font(), 36)
+clock = pygame.time.Clock()
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y):
         global S_PLAYER, S_PLAYER_DEAD
         super(Player, self).__init__()
-        width = int(width)
-        height = int(height)
 
         self.image = S_PLAYER
         self.image_original = self.image.copy()
@@ -45,18 +92,19 @@ class Player(pygame.sprite.Sprite):
         position_before = (self.x, self.y)
 
         if pressed_keys[K_w]:
-            self.y -= self.speed*time
+            self.y -= self.speed * time
 
         if pressed_keys[K_s]:
-            self.y += self.speed*time
+            self.y += self.speed * time
 
         if pressed_keys[K_a]:
-            self.x -= self.speed*time
+            self.x -= self.speed * time
 
         if pressed_keys[K_d]:
-            self.x += self.speed*time
+            self.x += self.speed * time
 
-        if self.x + self.rect.width > SCREEN_SIZE[0] or self.x < 0 or self.y+self.rect.height > SCREEN_SIZE[1] or self.y < 0:
+        if self.x + self.rect.width > SCREEN_SIZE[0] or self.x < 0 or self.y + self.rect.height > SCREEN_SIZE[
+            1] or self.y < 0:
             self.x, self.y = position_before
 
         self.rect.x, self.rect.y = int(self.x), int(self.y)
@@ -65,7 +113,7 @@ class Player(pygame.sprite.Sprite):
             self.rect.x, self.rect.y = int(self.x), int(self.y)
 
         self.angle = get_angle(pygame.mouse.get_pos(), self.rect.center)
-        self.image = pygame.transform.rotate(self.image_original, -self.angle+90)
+        self.image = pygame.transform.rotate(self.image_original, -self.angle + 90)
 
     def take_damage(self, dmg):
         if self.hp <= 0:
@@ -74,19 +122,17 @@ class Player(pygame.sprite.Sprite):
         self.hp -= dmg
 
         if self.hp <= 0:
-            self.image = pygame.transform.rotate(self.image_dead_original, -self.angle+90)
-            # TODO: send to server
+            self.image = pygame.transform.rotate(self.image_dead_original, -self.angle + 90)
+            send_to_server(client, {"code": "PLAYER_DEAD"}, (HOST, PORT))
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height, addr):
+    def __init__(self, x, y, addr):
         global S_ENEMY, S_ENEMY_DEAD
         super(Enemy, self).__init__()
-        width = int(width)
-        height = int(height)
 
         self.image = S_ENEMY
         self.image_original = self.image.copy()
@@ -107,31 +153,29 @@ class Enemy(pygame.sprite.Sprite):
         if self.died:
             return
 
-        coords = data[self.addr]["coords"]
+        coordinates = data[self.addr]["coords"]
 
-        self.x = coords[0]
-        self.y = coords[1]
+        self.x = coordinates[0]
+        self.y = coordinates[1]
 
         self.rect.x = int(self.x)
         self.rect.y = int(self.y)
         self.angle = data[self.addr]["angle"]
 
-        self.image = pygame.transform.rotate(self.image_original, -self.angle+90)
+        self.image = pygame.transform.rotate(self.image_original, -self.angle + 90)
 
     def dead(self):
         self.died = True
-        self.image = pygame.transform.rotate(self.image_dead_original, -self.angle+90)
+        self.image = pygame.transform.rotate(self.image_dead_original, -self.angle + 90)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
 
 class Wall(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y):
         global S_WALL
         super(Wall, self).__init__()
-        width = int(width)
-        height = int(height)
 
         self.image = S_WALL
 
@@ -149,7 +193,7 @@ class Bullet(pygame.sprite.Sprite):
 
         self.image = S_BULLET
         self.image_original = self.image.copy()
-        self.image = pygame.transform.rotate(self.image, -angle+90)
+        self.image = pygame.transform.rotate(self.image, -angle + 90)
 
         self.x = x
         self.y = y
@@ -167,8 +211,8 @@ class Bullet(pygame.sprite.Sprite):
         screen.blit(self.image, self.rect)
 
     def update(self, time, walls, player, addr):
-        self.x += sin(radians(-self.angle-90)) * time * self.speed
-        self.y += cos(radians(-self.angle-90)) * time * self.speed
+        self.x += sin(radians(-self.angle - 90)) * time * self.speed
+        self.y += cos(radians(-self.angle - 90)) * time * self.speed
 
         self.rect.x = int(self.x)
         self.rect.y = int(self.y)
@@ -213,29 +257,176 @@ class Health(pygame.sprite.Sprite):
             bonuses.remove(self)
 
 
+system("CLS")
+
+# Connecting to server
+nickname = ""
+while len(nickname) > 15 or len(nickname) <= 0:
+    nickname = input("Enter your name (max. 15 chars): ")
+
+HOST, PORT = 'localhost', 7777
+
+print(f"Connecting to server at {HOST}:{PORT}")
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.setblocking(False)
+send_to_server(client, {"code": "CONNECT_REQUEST", "nickname": nickname}, (HOST, PORT))
+
+GAME_NOT_STARTED = True
+while GAME_NOT_STARTED:
+    data = receive_from_server(client)
+
+    if not data:
+        continue
+
+    if data["code"] == "CONNECT_OK":
+        print("Successfully connected\n\nWaiting for players")
+
+        while True:
+            data = receive_from_server(client)
+
+            if not data or data["code"] != "START_GAME":
+                continue
+
+            game_map = data["map"]
+            player_address = data["player_address"]
+            main_player = data["player_coords"]
+            enemies_data = data["enemies"]
+            GAME_NOT_STARTED = False
+            break
+
+    else:
+        print("Can't connect")
+        sys.exit()
+
 # Groups of objects
 walls = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
 bonuses = pygame.sprite.Group()
 
+# Map Settings-
+map_size = [len(game_map[0]), len(game_map)]  # Size of map
+tile_size = [int(SCREEN_SIZE[0] / map_size[0]), int(SCREEN_SIZE[1] / map_size[1])]  # Size of one tile
+
 # Sprites
 S_BACKGROUND = pygame.transform.scale(pygame.image.load("sprites/background.png"), SCREEN_SIZE)
 S_WALL_ORIGINAL = pygame.image.load("sprites/wall.png")
-# TODO: resize wall (S_WALL = )
+S_WALL = pygame.transform.scale(S_WALL_ORIGINAL, tile_size)
 
 S_HEALTH = pygame.image.load("sprites/bonus_hp.png")
-S_HEALTH = pygame.transform.scale(S_HEALTH, (int(S_HEALTH.get_width() * (S_WALL.get_width()/S_WALL_ORIGINAL.get_width())), int(S_HEALTH.get_height() * (S_WALL.get_width()/S_WALL_ORIGINAL.get_width()))))
+S_HEALTH = pygame.transform.scale(S_HEALTH, (
+    int(S_HEALTH.get_width() * (S_WALL.get_width() / S_WALL_ORIGINAL.get_width())),
+    int(S_HEALTH.get_height() * (S_WALL.get_width() / S_WALL_ORIGINAL.get_width()))))
 
 S_BULLET = pygame.image.load("sprites/bullet.png")
-S_BULLET = pygame.transform.scale(S_BULLET, (int(S_BULLET.get_width() * (S_WALL.get_width()/S_WALL_ORIGINAL.get_width())), int(S_BULLET.get_height() * (S_WALL.get_width()/S_WALL_ORIGINAL.get_width()))))
+S_BULLET = pygame.transform.scale(S_BULLET, (
+    int(S_BULLET.get_width() * (S_WALL.get_width() / S_WALL_ORIGINAL.get_width())),
+    int(S_BULLET.get_height() * (S_WALL.get_width() / S_WALL_ORIGINAL.get_width()))))
 
 S_PLAYER = pygame.image.load("sprites/player.png")
-S_PLAYER = pygame.transform.scale(S_PLAYER, (int(S_PLAYER.get_width() * (S_WALL.get_width()/S_WALL_ORIGINAL.get_width())), int(S_PLAYER.get_height() * (S_WALL.get_width()/S_WALL_ORIGINAL.get_width()))))
+S_PLAYER = pygame.transform.scale(S_PLAYER, (
+    int(S_PLAYER.get_width() * (S_WALL.get_width() / S_WALL_ORIGINAL.get_width())),
+    int(S_PLAYER.get_height() * (S_WALL.get_width() / S_WALL_ORIGINAL.get_width()))))
 S_PLAYER_DEAD = pygame.image.load("sprites/player_dead.png")
-S_PLAYER_DEAD = pygame.transform.scale(S_PLAYER_DEAD, (int(S_PLAYER_DEAD.get_width() * (S_WALL.get_width()/S_WALL_ORIGINAL.get_width())), int(S_PLAYER_DEAD.get_height() * (S_WALL.get_width()/S_WALL_ORIGINAL.get_width()))))
+S_PLAYER_DEAD = pygame.transform.scale(S_PLAYER_DEAD, (
+    int(S_PLAYER_DEAD.get_width() * (S_WALL.get_width() / S_WALL_ORIGINAL.get_width())),
+    int(S_PLAYER_DEAD.get_height() * (S_WALL.get_width() / S_WALL_ORIGINAL.get_width()))))
 
 S_ENEMY = pygame.image.load("sprites/enemy.png")
-S_ENEMY = pygame.transform.scale(S_ENEMY, (int(S_ENEMY.get_width() * (S_WALL.get_width()/S_WALL_ORIGINAL.get_width())), int(S_ENEMY.get_height() * (S_WALL.get_width()/S_WALL_ORIGINAL.get_width()))))
+S_ENEMY = pygame.transform.scale(S_ENEMY, (
+    int(S_ENEMY.get_width() * (S_WALL.get_width() / S_WALL_ORIGINAL.get_width())),
+    int(S_ENEMY.get_height() * (S_WALL.get_width() / S_WALL_ORIGINAL.get_width()))))
 S_ENEMY_DEAD = pygame.image.load("sprites/enemy_dead.png")
-S_ENEMY_DEAD = pygame.transform.scale(S_ENEMY_DEAD, (int(S_ENEMY_DEAD.get_width() * (S_WALL.get_width()/S_WALL_ORIGINAL.get_width())), int(S_ENEMY_DEAD.get_height() * (S_WALL.get_width()/S_WALL_ORIGINAL.get_width()))))
+S_ENEMY_DEAD = pygame.transform.scale(S_ENEMY_DEAD, (
+    int(S_ENEMY_DEAD.get_width() * (S_WALL.get_width() / S_WALL_ORIGINAL.get_width())),
+    int(S_ENEMY_DEAD.get_height() * (S_WALL.get_width() / S_WALL_ORIGINAL.get_width()))))
+
+print(f"Game started. Player address: {player_address}")
+main_player = Player(main_player[0] * tile_size[0], main_player[1] * tile_size[1])
+
+# Parsing and building map
+for yi, y in enumerate(game_map):
+    for xi, x in enumerate(y):
+        if x == 1:
+            walls.add(Wall(tile_size[0] * xi, tile_size[1] * yi))
+
+for e in enemies_data:
+    print(e)
+    if e[0] == player_address:
+        continue
+    enemies.add(Enemy(e[1][0] * tile_size[0], e[1][1] * tile_size[1], e[0]))
+
+# Game loop
+while RUNNING:
+    new_bullets = []
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            RUNNING = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if not main_player.hp <= 0:
+                new_bullets.append(
+                    (main_player.rect.center[0], main_player.rect.center[1], 8, 14, main_player.angle, player_address))
+
+    # Sending current info to server
+    main_player.update(elapsed, pygame.key.get_pressed(), walls)
+    send_to_server(client, {"code": None, "coords": (main_player.x, main_player.y), "angle": main_player.angle,
+                            "bullets": new_bullets, "hp": main_player.hp}, (HOST, PORT))
+
+    # Getting new info from server
+    newest_data = None
+    server_new_bullets = []
+    keep = True
+    while keep:
+        data = receive_from_server(client)
+
+        if data:
+            newest_data = data
+            if data.get("bullets") and len(data["bullets"]) != 0:
+                server_new_bullets.extend(data["bullets"])
+
+            if data["code"] == "PLAYER_DEAD":
+                for e in enemies:
+                    if e.addr == data["addr"]:
+                        e.dead()
+
+            elif data["code"] == "GAME_FINISHED":
+                win_screen(screen, data["winner"])
+                RUNNING = False
+
+            if data.get("regen"):
+                bonuses.add(Health(*data['regen']))
+
+        else:
+            keep = False
+
+    if newest_data:
+        enemies.update(newest_data)
+
+        for b in server_new_bullets:
+            bullets.add(Bullet(*b))
+
+    bullets.update(elapsed, walls, main_player, player_address)
+    bonuses.update(main_player, enemies)
+
+    # Drawing
+    screen.blit(S_BACKGROUND, (0, 0))
+    bonuses.draw(screen)
+
+    walls.draw(screen)
+    enemies.draw(screen)
+    bullets.draw(screen)
+    main_player.draw(screen)
+
+    # HP interface
+    text_surface = font.render(f'{main_player.hp}', True, (255, 255, 255))
+    back = pygame.Surface((text_surface.get_width(), text_surface.get_height()))
+    back.blit(text_surface, (0, 0))
+    screen.blit(back, (15, 15))
+
+    pygame.display.flip()
+
+    elapsed = clock.tick(FPS) / 1000
+    pygame.display.set_caption(str(elapsed))
+
+pygame.quit()
--- server.py
+++ server.py
@@ -1,47 +1,121 @@
+import sys
 import socket
+import threading
 import pickle
+import random
 from os import system
+from copy import deepcopy
+
+BONUS_HP_CHANCE = 0.0008 / 100  # 0.00001% per frame
+
+
+def receive_data_from_players(server, game_data, players):
+    print("Started receiving data from players")
+    while True:
+        data, addr = receive_from_client(server)
+        if data["code"] == "PLAYER_DEAD":
+            print(f"Player {addr} died")
+
+            for p in players:
+                send_to_client({"code": "PLAYER_DEAD", "addr": addr}, p[0])
+
+        else:
+            game_data[addr] = data
+            game_data["bullets"].extend(data["bullets"])
+
+
+def send_new_data_to_players(data, players):
+    global GAME_END
+
+    print("Started sending data to players")
+    bullets_sent = False
+
+    while True:
+        is_winner = {p: True if d["hp"] > 0 else False for p, d in
+                     {k: v for k, v in data.items() if k != "bullets"}.items()}
+
+        # Check if someone won
+        if list(is_winner.values()).count(True) == 1:
+            winner = None
+            for p, w in is_winner.items():
+                if w:
+                    for pl in players:
+                        if pl[0] == p:
+                            winner = pl[2]
+                    break
+
+            for p in players:
+                send_to_client({"code": "GAME_FINISHED", "winner": winner}, p[0])
+
+            print(f"Player {winner} won")
+            GAME_END = True
+            sys.exit()
+
+        # Bonuses
+        bonuses = {}
+
+        if random.random() <= BONUS_HP_CHANCE:
+            bonuses["regen"] = (random.randint(tile_size[0], (map_size[0] - 1) * tile_size[0]),
+                                random.randint(tile_size[1], (map_size[1] - 1) * tile_size[0]))
+
+        # Send current game data
+        if len(data["bullets"]) != 0:
+            bullets_sent = True
+
+        gd = deepcopy(data)  # Copy data before sending for sync
+        for p in players:
+            send_to_client({"code": None, **gd, **bonuses}, p[0])
+
+        if bullets_sent:
+            data["bullets"] = []
+            bullets_sent = False
 
 
 def send_to_client(data, client):
     server.sendto(pickle.dumps(data), client)
 
 
-def recieve_from_client(server):
+def receive_from_client(server):
     data, addr = server.recvfrom(2048)
-    return (pickle.loads(data), addr)
+    return pickle.loads(data), addr
 
 
 HOST, PORT = 'localhost', 7777
-GAMEEND = False
+GAME_END = False
 
 addr = (HOST, PORT)
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(addr)
 
-# -------------------------------------------------------
-
-# Map of the game. ALWAYS SAME SIZE (10x10, 20x20, 30x30 etc.)
+# Map of the game. Must be same size (10x10, 20x20, 30x30 etc.)
 # # - wall
 #   - void
 # X - player
 mp = '''
-##########
-#    #   #
-#  X #   #
-#        #
-#        #
-#  #######
-#  #     #
-#  #     #
-#    X   #
-#        #
-##########
+####################
+#     #    #   X   #
+#  X  #    #       #
+#                  #
+#                  #
+#  #################
+#  #               #
+#  #               #
+#  # X             #
+#                  #
+#  #               #
+#  ####### #########
+#                  #
+#                  #
+#                  #
+#   ################
+#                  #
+#           X      #
+#                  #
+####################
 '''
 
 mp = mp.strip()
-
-gamemap = []
+game_map = []
 
 for yi, y in enumerate(mp.split("\n")):
     cur_y = []
@@ -54,28 +128,47 @@ for yi, y in enumerate(mp.split("\n")):
         elif x.lower() == 'x':
             cur_y.append(5)
 
-    gamemap.append(cur_y)
+    game_map.append(cur_y)
 
-mapsize = [len(gamemap[0]), len(gamemap)]
+map_size = [len(game_map[0]), len(game_map)]
 SCREEN_SIZE = [600, 600]
-tilesize = [int(SCREEN_SIZE[0]/mapsize[0]), int(SCREEN_SIZE[1]/mapsize[1])]
+tile_size = [int(SCREEN_SIZE[0] / map_size[0]), int(SCREEN_SIZE[1] / map_size[1])]
 
-players_coords = []
+players_coordinates = []
 
-for yi, y in enumerate(gamemap):
+for yi, y in enumerate(game_map):
     for xi, x in enumerate(y):
         if x == 5:
-            players_coords.append((xi, yi))
+            players_coordinates.append((xi, yi))
 
 system("CLS")
-# -------------------------------------------------------
 
 print("Server started, waiting for players...")
 PLAYERS = []
 
-
-while len(PLAYERS) < len(players_coords):
-    data, addr = recieve_from_client(server)
+while len(PLAYERS) < len(players_coordinates):
+    data, addr = receive_from_client(server)
     print(f'Player connected {addr}')
-    PLAYERS.append((addr, players_coords[len(PLAYERS)], data["nickname"]))
+    PLAYERS.append((addr, players_coordinates[len(PLAYERS)], data["nickname"]))
+
     send_to_client({"code": "CONNECT_OK", "ip": addr}, addr)
+
+print("\n\nAll players are connected. Game started")
+
+for player in PLAYERS:
+    send_to_client({"code": "START_GAME", "map": game_map, "player_coords": player[1], "player_address": player[0],
+                    "enemies": PLAYERS}, player[0])
+
+GAME_DATA = {"bullets": []}
+for p in PLAYERS:
+    GAME_DATA[p[0]] = {"coords": p[1], "angle": 0, "hp": 100}
+
+print(PLAYERS)
+recv_thread = threading.Thread(target=receive_data_from_players, args=(server, GAME_DATA, PLAYERS))
+send_thread = threading.Thread(target=send_new_data_to_players, args=(GAME_DATA, PLAYERS))
+
+recv_thread.start()
+send_thread.start()
+
+if GAME_END:
+    server.close()
